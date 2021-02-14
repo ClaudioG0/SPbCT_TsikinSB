1dd715be8d49657a44ae795cf6d08f19ce7545e4
ClaudioG0 <51310866+ClaudioG0@users.noreply.github.com>
2021-02-14 21:45:27 +0300

    Update itog_insertion_sort.java

--- PZ_4/src/itog_insertion_sort.java
+++ PZ_4/src/itog_insertion_sort.java
@@ -1,119 +1,123 @@
 import java.util.Arrays;
-
-public class itog_insertion_sort {
-        public static void main(String[] args)
-        {
-            Integer nums[];
-            int i = -1;
-            Integer y ;
-            int j ;
-            nums = new Integer [ 1 ];
-            nums = Put(90,nums ,i = in(i)) ; // Пример добовление числа в очередь и динамическое увеличение масcива
-            nums = Put(-9,nums ,i = in(i)) ;
-            nums = Put(420,nums ,i = in(i)) ;
-            nums = Put(11,nums ,i = in(i)) ;
-            nums = Put(6,nums ,i = in(i)) ;
-            nums = Put(99,nums ,i = in(i)) ;
-            nums = Put(1003,nums ,i = in(i)) ;
-            nums = Put(200,nums ,i = in(i)) ;
-
-            System.out.print(Arrays.toString(nums));
-            System.out.print('\n');
-            System.out.println("--------------------— Ввод данных успешен —-------------------------------");
-            y = Get(nums) ;
-            System.out.println(y);
-            y = Get(nums) ;
-            System.out.println(y);
-
-            System.out.println("--------------------— Доставление из очереди прошло успешно —--------------------");
-            System.out.println(nums.length);
-            System.out.println(nums);
-
-            nums = GetAll(nums); // Очищаем пустые ячейки масива
-            System.out.println('\n');
-
-            System.out.println(nums.length); // Вывод для проверки
-            System.out.println(nums);
+import java.util.*;
 
 
-            System.out.println("---------------— Очистка масива от пустых полей успешна —----------------");
-            int R[] = new int [ nums.length ];
+public class test3 {
 
-            for ( j = 0; j < nums.length ; j++)
-            {
+    int size;
 
-                R[j] = (nums[j]) ; // Преобразование масива из String[] в Int[]
-            }
-            R = insertionSortImperative(R); // Вызываем метод для сортировки по варианту 0 (Пузырьковая сортировка)
-
-            for ( j = 0; j < R.length ; j++){
-                System.out.println(R[j]); // Выводим массив для проверки сортировки
-            }
-            System.out.println("----------— Работа сортировки успешна (1 индивидуальное задание ) —---------");
-
-        }
-        static Integer[] Put (Integer x, Integer[] y, int i)
-        {
-            if (y.length <= i )
-            {
-
-                Integer PLM[] = new Integer [y.length * 2 ];
-
-                for (int j = 0; j < y.length; j++){
-                    PLM[j] = y[j];
-                }
-                y = PLM;
-            }
-
-            y[i] = x ;
-            return y;
-        }
-        static int in (int x )
-        {
-            x++;
-            return x ;
-        }
-        static Integer Get (Integer y[] )
-        {
-            return y[0] ;
-        }
-
-        static Integer[] GetAll (Integer y[] )
-        {
-            int k = 0 ;
-            for (int i = 0 ; i < y.length; i++)
-            {
-                if (y[i] == null )
-                {
-                    k++;
-                }
-            }
-            Integer PLM[] = new Integer [ (y.length - k) ];
-
-            for (int j = 0; j < PLM.length; j++){
-                PLM[j] = y[j];
-            }
-            return PLM;
-        }
-        public static int[] insertionSortImperative(int[]array)
-        {
-            for (int left = 0; left < array.length; left++) {
-                // Вытаскиваем значение элемента
-                int value = array[left];
-                // Перемещаемся по элементам, которые перед вытащенным элементом
-                int i = left - 1;
-                for (; i >= 0; i--) {
-                    // Если вытащили значение меньшее — передвигаем больший элемент дальше
-                    if (value < array[i]) {
-                        array[i + 1] = array[i];
-                    } else {
-                        // Если вытащенный элемент больше — останавливаемся
-                        break;
-                    }
-                }
-                // В освободившееся место вставляем вытащенное значение
-                array[i + 1] = value;
-            }
-            return array;
-        }
+    public test3(int size) {
+        this.size = size;
     }
+
+
+    public static void main(String[] args) {
+        test3 arrSize = new test3(8);
+        int[] array = new int[arrSize.size];
+
+        System.out.println("Задание 1 - вариант 3.");
+
+        System.out.println("Создаем очередь из 8 элементов");
+        System.out.println(Arrays.toString(array));
+        System.out.println("\n");
+
+        array = Put(array, 56);
+        array = Put(array, 65);
+        array = Put(array, 3);
+        array = Put(array, 14);
+        array = Put(array, 55);
+        array = Put(array, 22);
+        array = Put(array, 22);
+        array = Put(array, 128);
+
+        System.out.println("добавили элементы в очередь");
+        System.out.println(GetAll(array));
+        System.out.println("\n");
+
+
+        System.out.println("получили элемент из начала очереди");
+        System.out.println(array[0]);
+        array = Get(array);
+        System.out.println(GetAll(array));
+        System.out.println("\n");
+
+
+        System.out.println("Отсортировали очередь по возрастанию методом вставками");
+        array = insertionSort(array);
+        System.out.println(GetAll(array));
+        System.out.println("\n");
+
+
+
+        System.out.println("Задание 2 - вариант 2. генерация массива из 30 элементов," +
+                            "  и вывод среднего значения");
+        int[] C = new int[30];
+        int max = 20;
+        for (int i = 0 ; i < C.length; i++) {
+            int rnd = rnd(max);
+            C[i] = rnd;
+        }
+        System.out.println(Arrays.toString(C));
+        int sum = 0;
+        for (int i = 0; i < C.length; i++) {
+            sum = sum + C[i];
+        }
+        System.out.println((double) sum/C.length);
+    }
+
+    public static int[] Put(int[] x, int y){
+        int arrayLength = x.length;
+        if (x[x.length-1] != 0){
+            x = Arrays.copyOf(x, x.length * 2);
+            x[x.length - arrayLength] = y;
+        }else {
+            for(int i=0; i< x.length; i++){
+                if(x[i]==0){
+                    x[i] = y;
+                    break;
+                }
+            }
+        }
+        return x;
+    }
+
+    public static int[] Get(int[] array){
+        array = Arrays.copyOfRange(array, 1, array.length);
+        return array;
+    }
+
+
+
+    public static String GetAll(int[] array)
+    {
+        return Arrays.toString(array);
+    }
+
+    public static int[] insertionSort(int[]array)
+    {
+        for (int left = 0; left < array.length; left++) {
+            // Вытаскиваем значение элемента
+            int value = array[left];
+            // Перемещаемся по элементам, которые перед вытащенным элементом
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                // Если вытащили значение меньшее — передвигаем больший элемент дальше
+                if (value < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    // Если вытащенный элемент больше — останавливаемся
+                    break;
+                }
+            }
+            // В освободившееся место вставляем вытащенное значение
+            array[i + 1] = value;
+        }
+        return array;
+    }
+
+    public static int rnd(int max)
+    {
+        return (int) (Math.random() * ++max);
+    }
+
+}
