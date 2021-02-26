7a1f21fdf42b85b53ec75665f686e08f9f5a514b
ClaudioG0 <51310866+ClaudioG0@users.noreply.github.com>
2021-02-26 19:24:21 +0300

    Add files via upload

---
+++ PZ07-08/src/Program6.java
@@ -0,0 +1,36 @@
+public class Program6 {
+
+    public static void main(String[] args) {
+
+        Button button = new Button(new ButtonClickHandler());
+        button.click();
+        button.click();
+        button.click();
+    }
+}
+
+class ButtonClickHandler implements EventHandler{
+
+    public void execute(){
+
+        System.out.println("Кнопка нажата!");
+    }
+}
+
+interface EventHandler{
+
+    void execute();
+}
+
+class Button{
+
+    EventHandler handler;
+    Button(EventHandler action){
+
+        this.handler=action;
+    }
+    public void click(){
+
+        handler.execute();
+    }
+}
