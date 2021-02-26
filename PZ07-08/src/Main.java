ec451784733674d2d74354986d599d52b9a69d71
ClaudioG0 <51310866+ClaudioG0@users.noreply.github.com>
2021-02-26 19:53:58 +0300

    Add files via upload

---
+++ PZ07-08/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Car с1 = new Car("Mercedes-Benz", "S-klasse", "s500", 7000000, 2015);
+        с1.println();
+        Car с2 = new Car("BMW", "7 Series", "750 Li", 7050000, 2016);
+        с2.println();
+        Car с3 = new Car("Audi", "A8", "Long", 7450000, 2016);
+        с3.println();
+    }
+}
